c7da46d84f9057cfab4ab4280b74a6b0a0ac03ce
kabore-226 <kaboreissouf00226@gmail.com>
2026-05-11 08:18:49 +0000

    Structure complète frontend + backend

---
+++ barka_backend/src/main/java/com/barka/barka_backend/BarkaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.barka.barka_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarkaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarkaBackendApplication.class, args);
+	}
+
+}
---
+++ barka_backend/src/test/java/com/barka/barka_backend/BarkaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barka.barka_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarkaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
